8c3afd3ceb5d1f862057381975e28f82c3af48a4
JEONGSU_JASON <43768492+jeongsuCalvin@users.noreply.github.com>
2024-09-30 17:04:54 -0700

    Update for function study
    240930 study update

--- #First Git for the study.py
+++ #First Git for the study.py
@@ -1,3 +1,43 @@
 #First Git for the study
 #For fucntion
-#2nd git for test
+#1 step for function
+test_list = ['one', 'two', 'three'] 
+for i in test_list: 
+     print(i)
+# for 'variable' in 'list'
+for i in ['one','two','three']:
+     print (i)
+# it is avaliable put list directly into for list
+a = [(1,2), (3,4), (5,6)]
+for (first, last) in a:
+     print(first + last)
+# (1,2) -> (first, last)
+marks=[80,25,37,90,100]
+number = 0
+for mark in marks:
+     number = number+1
+     if mark>= 60:
+          print("Student %d is passed." %number)
+     else:
+          print ("Student %d is failed"%number)
+#
+marks=[80,25,37,90,100]
+number = 0
+for mark in marks:
+     number = number+1
+     if mark<= 60:
+          continue
+     print("Student %d is passed." %number)
+# continue = pass and go to next for loop
+add=0
+for i in range (1,11):
+     add = add + i
+
+print (add)
+# range(a,b) - a~(b-1) range object
+# range (a) - 0~(a-1)
+marks=[80,25,37,90,100]
+for number in range(len(marks)):
+    if marks[number] < 60:
+         continue
+    print ("Student %d is passed" %(number+1))
